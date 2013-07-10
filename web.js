3ff70850b6a0778feecbd10287e6838dc1a40cda
Kevin Chow <kevinckw@gmail.com>
2013-07-10 06:52:01 +0000

    Modified to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+// Read the content from the file
+var readContent = fs.readFileSync('index.html');
+
+// Convert the buffer into a string
+var readStr = readContent.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readStr);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
